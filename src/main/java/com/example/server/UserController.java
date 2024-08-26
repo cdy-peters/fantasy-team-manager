b7b65701408be2cd94b1e76813648e935d628585
Cody <cjosephp15@gmail.com>
2024-08-27 09:11:47 +1000

    Added password hashing

--- src/main/java/com/example/server/UserController.java
+++ src/main/java/com/example/server/UserController.java
@@ -1,5 +1,6 @@
 package com.example.server;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,17 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     private UserDAO userDAO = new UserDAO();
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @PostMapping("/login")
     public void login(@RequestBody User user) {
         String username = user.getUsername();
 
         User existingUser = userDAO.findByUsername(username);
-        System.out.println(existingUser.getEmail());
+
+        Boolean passwordMatch = passwordEncoder.matches(user.getPassword(), existingUser.getPassword());
+        if (passwordMatch) {
+            System.out.println("Login successful");
+        } else {
+            System.out.println("Login failed");
+        }
     }
 
     @PostMapping("/register")
     public void register(@RequestBody User user) {
+        // Hash password
+        String password = user.getPassword();
+        String hashedPassword = passwordEncoder.encode(password);
+        user.setPassword(hashedPassword);
+
         userDAO.create(user);
     }
 

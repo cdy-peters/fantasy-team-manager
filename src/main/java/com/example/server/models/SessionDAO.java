66cb777e38949650e0db0a634d6a6f6c55277c27
Cody <cjosephp15@gmail.com>
2024-10-01 19:54:52 +1000

    feat: Added getUserId method to SessionDAO

--- src/main/java/com/example/server/models/SessionDAO.java
+++ src/main/java/com/example/server/models/SessionDAO.java
@@ -1,14 +1,32 @@
 package com.example.server.models;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.Statement;
 
 import com.example.server.Server;
 
 import jakarta.servlet.http.HttpSession;
 
 public class SessionDAO {
+
+    public static Long getUserId(String sessionId) {
+        String query = String.format("SELECT user_id FROM session WHERE session_id = '%s'", sessionId);
+        try {
+            Statement stmt = Server.conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                return rs.getLong("user_id");
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public void create(HttpSession session) {
         String query = "INSERT INTO session (session_id, created_at, max_interval, user_id) VALUES (?, ?, ?, ?)";

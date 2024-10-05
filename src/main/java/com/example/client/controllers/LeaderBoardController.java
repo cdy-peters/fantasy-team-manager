e3ddf033c82b45778ee0949d11c9b25d28516e3d
Marcus Camaroni <psminchia4153@hotmail.com>
2024-10-05 21:28:52 +1000

    feat: populate leaderboard with data

--- src/main/java/com/example/client/controllers/LeaderBoardController.java
+++ src/main/java/com/example/client/controllers/LeaderBoardController.java
@@ -5,8 +5,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 import com.example.client.Client;
-import com.example.server.models.IUser;
-import com.example.server.models.IUserRoster;
+import com.example.server.models.ILeaderboardElement;
+import com.google.gson.Gson;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -18,28 +19,64 @@ import java.io.IOException;
 import java.net.URI;
 
 public class LeaderBoardController {
-    // @FXML
-    // private TableView<Player> leaderboardTable;
     @FXML
-    private TableColumn<IUser, String> nameColumn;
+    private TableView<ILeaderboardElement> leaderboardTable;
 
     @FXML
-    private TableColumn<IUserRoster, Integer> scoreColumn;
+    private TableColumn<ILeaderboardElement, Long> playerColumn;
 
-    private HttpClient httpClient = HttpClient.newHttpClient();
+    @FXML
+    private TableColumn<ILeaderboardElement, Integer> scoreColumn;
 
-    public void setHttpClient(HttpClient httpClient) {
-        this.httpClient = httpClient;
+    private ObservableList<ILeaderboardElement> data;
+    private HttpClient httpClient;
+    private Gson gson;
+
+    public LeaderBoardController() {
+        this.httpClient = HttpClient.newHttpClient();
+        this.gson = new Gson();
     }
 
-    private HttpRequest createHttpRequest(String position) {
+    @FXML
+    public void initialize() {
+        data = FXCollections.observableArrayList();
+
+        playerColumn.setCellValueFactory(new PropertyValueFactory<>("userId"));
+        scoreColumn.setCellValueFactory(new PropertyValueFactory<>("score"));
+
+        data.addAll(fetchPlayers());
+        leaderboardTable.setItems(data);
+    }
+
+    private ObservableList<ILeaderboardElement> fetchPlayers() {
+        try {
+            HttpRequest request = createHttpRequest();
+            HttpResponse<String> response = sendRequest(request);
+
+            System.out.println("Response: " + response.body());
+            ILeaderboardElement[] players = gson.fromJson(response.body(), ILeaderboardElement[].class);
+
+            // debug
+            for (ILeaderboardElement player : players) {
+                System.out.println("Player ID: " + player.getUserId() + ", Score: " + player.getScore());
+            }
+            // debug
+
+            return FXCollections.observableArrayList(players);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return FXCollections.observableArrayList();
+        }
+    }
+
+    private HttpRequest createHttpRequest() {
         return HttpRequest.newBuilder()
-                .uri(URI.create(Client.SERVER_URL + "/roster"))
+                .uri(URI.create(Client.SERVER_URL + "rosters"))
                 .header("Content-Type", "application/json")
                 .build();
     }
 
-    private HttpResponse <String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
+    private HttpResponse<String> sendRequest(HttpRequest request) throws IOException, InterruptedException {
         return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
